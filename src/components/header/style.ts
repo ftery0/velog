9403ee29d522fb9bf6d4ec48b5f8e6b7afc1b71f
해준 <127281057+ftery0@users.noreply.github.com>
2024-09-13 08:28:23 +0900

    chore :: 헤더추가 백업을 위한 커밋..

---
+++ src/components/header/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const Header = styled.header`
+    display: flex;
+    width: 100vw;
+    height: 100px;
+    background-color: antiquewhite;
+`
+export const HeaderMain = styled.div`
+
+`
